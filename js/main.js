0c53311dffdae6da88718024eeeae94d77941422
Vit <grumdrig@mail.ru>
2021-05-28 00:09:38 +0300

    3

--- js/main.js
+++ js/main.js
@@ -4,30 +4,59 @@ jQuery(document).ready(function($) {
     width: '55px'
   });
   //inputs check
-  var label = [$("[data-input]").parent().find('label').text()];
-  console.log(label);
-  $("[data-input]").change(function(){
+  var label = $("[data-input]").parent().find('label span').each(function() {$(this).text()})
+  var labelNews = $("[data-inputnews]").parent().find('label span').each(function() {$(this).text()})
+  
+  $("[data-input]").change(function (){
     $(this).each(function(){
-      input = $(this).val().trim();
+      input = $(this).val();
+      var parent = $(this).parent().index();
       if(input) {
-        var parent = $(this).parent().index();
-        console.log(parent);
-        $(this).parent().find('label').text(label[parent]);
-        console.log($(this).parent().eq($(this).index()).find('label').text(label[parent]));
+        $(this).parent().find('label').html(label[parent]);
+        $(this).parent().removeClass('alert');
+        $(this).parent().find('label').addClass('checked')
+      } else {
+        $(this).attr('placeholder','');
+        $(this).parent().find('label').removeClass('checked')
       }
     });
   });
-  $("[data-subm]").click(function(){
-    $("[data-input]").each(function(){
-      input = $(this).val().trim();
-      if(!input) {
-        $(this).parent().addClass('alert');
-        $(this).parent().find('label').text('Ошибка заполнения');
-        $(this).attr('placeholder','Обязательное поле')
-      } else {
+  $("[data-inputnews]").change(function (){
+    $(this).each(function(){
+      input = $(this).val();
+      var parent = $(this).parent().index();
+      if(input) {
+        $(this).parent().find('label').html(labelNews[parent]);
         $(this).parent().removeClass('alert');
+        $(this).parent().find('label').addClass('checked')
+      } else {
+        $(this).attr('placeholder','');
+        $(this).parent().find('label').removeClass('checked')
       }
-    })
+    });
+  });
+  $('.quest__inp-wrap > select').change(function(){
+    $(this).parent().find('label').addClass('checked');
+    $(this).parent().addClass('checked');
+  })
+  function requ(){
+    input = $(this).val();
+    if(!input) {
+      $(this).parent().addClass('alert');
+      $(this).parent().find('label').text('Ошибка заполнения');
+      $(this).attr('placeholder','Обязательное поле')
+    } else {
+      $(this).parent().removeClass('alert');
+      $(this).attr('placeholder','')
+    }
+  };
+  $("[data-subm]").click(function(){
+    $("[data-input]").each(requ);
+    $("[data-text]").each(requ);
+    $("[data-select]").each(requ);
+  });
+  $("[data-submnews]").click(function(){
+    $("[data-inputnews]").each(requ);
   });
   //tabs
 //   $('[data-can]').on('click', function() {
